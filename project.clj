e4302dbe049e4a590d731ec9c1d6a68f0a8a66d7
Erik Schwartz <erik@tablexi.com>
2012-08-13 09:28:39 -0500

    Complete through 14

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "The functional koans."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [koan-engine "0.1.2-SNAPSHOT"]]
+  :plugins [[lein-swank "1.4.4"]]
   :dev-dependencies [[swank-clojure "1.3.0" :exclusions [org.clojure/clojure]]
                      [lein-koan "0.1.0"]])
